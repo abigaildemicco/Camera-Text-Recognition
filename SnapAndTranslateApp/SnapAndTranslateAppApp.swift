26c80345a078d6ad3b07627891285f73242467ab
abigaildemicco <abdemicco@gmail.com>
2021-12-15 16:41:53 +0100

    Initial Commit

---
+++ SnapAndTranslateApp/SnapAndTranslateAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapAndTranslateAppApp.swift
+//  SnapAndTranslateApp
+//
+//  Created by Abigail De Micco on 15/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SnapAndTranslateAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
